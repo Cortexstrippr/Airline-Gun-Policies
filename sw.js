825508e6b17197a316d558fa7eb41b5808dd348e
Cortexstrippr <dogden44@gmail.com>
2026-07-10 17:09:23 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,10 +1,28 @@
-const cacheName = 'fly-gun-v2';
-const assets = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png', './atf-logo.png', './aa-logo.png'];
+const cacheName = 'fly-gun-v4';
+const assets = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png',
+  './atf-logo.png',
+  './aa-logo.png',
+  './alaska-logo.png',
+  './ccw-logo.png',
+  './delta-logo.png',
+  './hawaiian-logo.png',
+  './tsa-logo.png',
+  './united-logo.png'
+];
 
 self.addEventListener('install', e => {
-e.waitUntil(caches.open(cacheName).then(cache => cache.addAll(assets)));
+  e.waitUntil(caches.open(cacheName).then(cache => {
+    return cache.addAll(assets);
+  }));
 });
 
 self.addEventListener('fetch', e => {
-e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
+  e.respondWith(caches.match(e.request).then(response => {
+    return response || fetch(e.request);
+  }));
 });
